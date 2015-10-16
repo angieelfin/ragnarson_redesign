ce52aeb8a2df8ced49f4b237f3ed39d6a1f9555a
Agata Kubiak <aga.kubia@gmail.com>
2015-10-16 17:39:26 +0200

    Add zepto.js. Add moving ruby in What we do section.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,33 @@
 //= require_tree .
+$(document).ready(function(){
+
+
+  $(window).scroll(function(){
+
+    var wScroll = $(this).scrollTop();
+    var rubyIcon = $('.js-ruby-icon');
+
+    var sectionTop = $('.heading');
+    var sectionBottom = $('.section-info');
+
+    var pointTop = sectionTop.offset().top - sectionTop.height();
+    var pointBottom = sectionBottom.offset().top - sectionBottom.height();
+
+    console.log(wScroll);
+    if (wScroll <= pointTop) {
+      rubyIcon
+      .addClass('static-top');
+    } else if (wScroll > pointTop && wScroll < pointBottom) {
+      rubyIcon
+      .removeClass('static-top')
+      .removeClass('static-bottom')
+      .addClass('fixed');
+    }
+    else if (wScroll >= pointBottom) {
+      rubyIcon
+      .removeClass('fixed')
+      .addClass('static-bottom');
+    }
+  });
+
+});
